4c42d4f4c85ea370bc79039c31c87026119fdc0c
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-03-11 16:00:41 +0100

    add files

---
+++ script.js
@@ -0,0 +1,12 @@
+var name = prompt('Enter your name'); // prompt dialog box
+alert('How are you today ' + name + '?'); //alert dialog box
+
+if(confirm('Do you wan to continue?')){ //confirm dialog box
+    alert("So go on!");
+}else{
+    alert("Goodbay");
+}
+
+/*
+console.log('How are you today ' + name + '?');
+*/
